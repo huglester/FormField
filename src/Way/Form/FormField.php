b665db2bae768a0c617ae1c9df0d874d2e8b5382
Jaroslav Petrusevic <huglester@gmail.com>
2014-08-07 14:29:06 +0000

    some improvements

--- src/Way/Form/FormField.php
+++ src/Way/Form/FormField.php
@@ -4,127 +4,190 @@ use Form, Config;
 
 class FormField {
 
-    /**
-     * Instance
-     *
-     * @var Way\Form\FormField
-     */
-    protected static $instance;
+	/**
+	 * Instance
+	 *
+	 * @var Way\Form\FormField
+	 */
+	protected static $instance;
 
-    /**
-     * Make the form field
-     *
-     * @param string $name
-     * @param array $args
-     */
-    public function make($name, array $args)
-    {
-        $wrapper = $this->createWrapper();
-        $field = $this->createField($name, $args);
+	/**
+	 * Make the form field
+	 *
+	 * @param string $name
+	 * @param array  $args
+	 *
+	 * @return mixed
+	 */
+	public function make($name, array $args)
+	{
+		$wrapper = $this->createWrapper();
+		$field   = $this->createField($name, $args);
 
-        return str_replace('{{FIELD}}', $field, $wrapper);
-    }
+		return str_replace('{{FIELD}}', $field, $wrapper);
+	}
 
-    /**
-     * Prepare the wrapping container for
-     * each field.
-     */
-    protected function createWrapper()
-    {
-        $wrapper = Config::get('form::wrapper');
-        $wrapperClass = Config::get('form::wrapperClass');
+	/**
+	 * Prepare the wrapping container for
+	 * each field.
+	 */
+	protected function createWrapper()
+	{
+		$wrapper      = Config::get('form::wrapper');
+		$wrapperClass = Config::get('form::wrapperClass');
 
-        return '<' . $wrapper . ' class="' . $wrapperClass . '">{{FIELD}}</' . $wrapper . '>';
-    }
+		return '<'.$wrapper.' class="'.$wrapperClass.'">{{FIELD}}</'.$wrapper.'>';
+	}
 
-    /**
-     * Create the form field
-     *
-     * @param string $name
-     * @param array $args
-     */
-    protected function createField($name, $args)
-    {
-        // If the user specifies an input type, we'll just use that.
-        // Otherwise, we'll take a best guess approach.
-        $type = array_get($args, 'type') ?: $this->guessInputType($name);
+	/**
+	 * Create the form field
+	 *
+	 * @param string $name
+	 * @param array  $args
+	 *
+	 * @return string
+	 */
+	protected function createField($name, $args)
+	{
+		// If the user specifies an input type, we'll just use that.
+		// Otherwise, we'll take a best guess approach.
+		$type = array_get($args, 'type') ?: $this->guessInputType($name);
 
-        // We'll default to Bootstrap-friendly input class names
-        $args = array_merge(['class' => Config::get('form::inputClass')], $args);
+		// We'll default to Bootstrap-friendly input class names
+		$args = array_merge(['class' => Config::get('form::inputClass')], $args);
 
-        $field = $this->createLabel($args, $name);
+		$field = $this->createLabel($args, $name);
 
-        unset($args['label']);
+		unset($args['label']);
 
-        return $field .= $this->createInput($type, $args, $name);
-    }
+		return $field .= $this->createInput($type, $args, $name);
+	}
 
-    /**
-     * Handle of creation of the label
-     *
-     * @param array $args
-     * @param string $name
-     */
-    protected function createLabel($args, $name)
-    {
-        $label = array_get($args, 'label');
+	/**
+	 * Handle of creation of the label
+	 *
+	 * @param array  $args
+	 * @param string $name
+	 *
+	 * @return string
+	 */
+	protected function createLabel($args, $name)
+	{
+		$label = array_get($args, 'label');
 
-        // If no label was provided, let's do our best to construct
-        // a label from the method name.
-        is_null($label) and $label = $this->prettifyFieldName($name) . ': ';
+		// If no label was provided, let's check out conrigs, to see if there
+		// are some defaults
+		is_null($label) and $label = $this->checkLabelConfig($name);
 
-        return $label ? Form::label($name, $label, array('class' => 'control-label')) : '';
-    }
+		// If no label was provided, let's do our best to construct
+		// a label from the method name.
+		is_null($label) and $label = $this->prettifyFieldName($name).': ';
 
-    /**
-     * Manage creation of input
-     *
-     * @param string $type
-     * @param array $args
-     * @param string $name
-     */
-    protected function createInput($type, $args, $name)
-    {
-        return $type == 'password'
-            ? Form::password($name, $args)
-            : Form::$type($name, null, $args);
-    }
+		return $label ? Form::label($name, $label, array('class' => Config::get('form::labelClass'))) : '';
+	}
 
-    /**
-     * Provide a best guess for what the
-     * input type should be.
-     *
-     * @param string $name
-     */
-    protected function guessInputType($name)
-    {
-        return array_get(Config::get('form::commonInputsLookup'), $name) ?: 'text';
-    }
+	/**
+	 * Manage creation of input
+	 *
+	 * @param string $type
+	 * @param array  $args
+	 * @param string $name
+	 * @param array  $options
+	 *
+	 * @return string
+	 */
+	protected function createInput($type, $args, $name)
+	{
+		if (isset($args['value']) and $type != 'password')
+		{
+			$value = $args['value'];
+			unset($args['value']);
+		} else
+		{
+			$value = null;
+		}
 
-    /**
-     * Clean up the field name for the label
-     *
-     * @param string $name
-     */
-    protected function prettifyFieldName($name)
-    {
-        return ucwords(preg_replace('/(?<=\w)(?=[A-Z])/', " $1", $name));
-    }
 
-    /**
-     * Handle dynamic method calls
-     *
-     * @param string $name
-     * @param array $args
-     */
-    public static function __callStatic($name, $args)
-    {
-        $args = empty($args) ? [] : $args[0];
+		if ($type === 'select')
+		{
+			$options = [];
+			if (isset($args['options']))
+			{
+				$options = $args['options'];
+				unset($args['options']);
+			}
 
-        $instance = static::$instance;
-        if ( ! $instance) $instance = static::$instance = new static;
+			return Form::select($name, $options, $value, $args);
+		}
+		elseif ($type === 'boolean')
+		{
+			$options = [1 => 'Yes', 0 => 'No'];
+			if (isset($args['options']))
+			{
+				$options = $args['options'];
+				unset($args['options']);
+			}
 
-        return $instance->make($name, $args);
-    }
+			return Form::select($name, $options, $value, $args);
+		}
+
+
+		return $type == 'password'
+			? Form::password($name, $args)
+			: Form::$type($name, $value, $args);
+	}
+
+	/**
+	 * Provide a best guess for what the
+	 * input type should be.
+	 *
+	 * @param string $name
+	 *
+	 * @return string
+	 */
+	protected function guessInputType($name)
+	{
+		return array_get(Config::get('form::commonInputsLookup'), $name) ?: 'text';
+	}
+
+	/**
+	 * Clean up the field name for the label
+	 *
+	 * @param string $name
+	 *
+	 * @return string
+	 */
+	protected function prettifyFieldName($name)
+	{
+		return ucwords(preg_replace('/(?<=\w)(?=[A-Z])/', " $1", $name));
+	}
+
+	/**
+	 * Check if config has default label for a field
+	 *
+	 * @param string $name
+	 *
+	 * @return string
+	 */
+	protected function checkLabelConfig($name)
+	{
+		return array_get(Config::get('form::defaultLabels'), $name) ?: null;
+	}
+
+	/**
+	 * Handle dynamic method calls
+	 *
+	 * @param string $name
+	 * @param array  $args
+	 */
+	public static function __callStatic($name, $args)
+	{
+		$args = empty($args) ? [] : $args[0];
+
+		$instance = static::$instance;
+		if ( ! $instance) $instance = static::$instance = new static;
+
+		return $instance->make($name, $args);
+	}
 
 }
